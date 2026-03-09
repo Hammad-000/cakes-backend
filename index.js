db02f19ef72ca15427f1425f7eca9307232d228a
Hammad Ahmed <syedhammad283@gmail.com>
2026-03-09 17:53:52 +0500

    img upload

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import mongoose from "mongoose";
+import multer from "multer";
+import cloudinary from 'cloudinary'
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 
 dotenv.config(); 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());  
 
 const allowedOrigins = [
-  "http://localhost:3000", // Local development
-  process.env.FRONTEND_URL // Production URL (e.g., Vercel, Netlify)
+  "http://localhost:3000",
+  process.env.FRONTEND_URL 
 ].filter(Boolean);
+
+
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-// MongoDB setup
 const DB_URL = process.env.MONGO_URI;  
 const client = new MongoClient(DB_URL, {
   serverApi: {
@@ -55,6 +64,35 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 //-----------------------------------------
+const fileSchema = new mongoose.Schema(
+  {
+    originalName: String,
+    cloudinaryUrl: String,
+    cloudinaryId: String,
+    fileType: String,
+    fileSize: Number
+  },
+  { timestamps: true }
+);
+
+
+
+const File = mongoose.model("File", fileSchema);
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 }, 
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) cb(null, true);
+    else cb(new Error("Only image files allowed"), false);
+  }
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
